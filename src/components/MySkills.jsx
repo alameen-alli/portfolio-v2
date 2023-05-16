04a459c13309e30b75ff33921d9d1a6871c27fb7
unknown <allialameen8739@gmail.com>
2023-05-16 17:40:18 +0100

    update skill section

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -5,14 +5,15 @@ import jsLogo from '../icons/cdnlogo.com_javascript.svg';
 import tlLogo from '../icons/cdnlogo.com_tailwind-css.svg';
 import mongoLogo from '../icons/cdnlogo.com_mongodb-icon.svg';
 import nodeLogo from '../icons/cdnlogo.com_nodejs-icon.svg';
-import ReactLogo from '../icons/cdnlogo.com_react.svg'
+import ReactLogo from '../icons/cdnlogo.com_react.svg';
+import redux from '../icons/redux.svg';
 
 const MySkills = () => {
     return (
         <div className='skill-container relative mt-32'>
 
             <div className='skill-header text-center sm:text-right'>
-                <span className='text-gradient'>My Skills</span>
+                <span className='text-gradient'>My Technologies</span>
             </div>
 
             <div className='skill-grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
@@ -58,6 +59,18 @@ const MySkills = () => {
                     </div>
                     <div class="min-w-0 flex-1 flex flex-col">
                         <p class="truncatee">React.js</p>
+                        {/* <small class="truncate">(Dart, Flutter, Firebase)</small> */}
+                    </div>
+
+                </div>
+
+                <div className='skill-card px-4 py-2 border border-neutral-900/10 dark:border-neutral-50/10 hover:border-neutral-900/30 dark:hover:border-neutral-50/30 rounded flex items-center gap-4 transition-colors duration-700 hover:duration-100 m-6'>
+
+                    <div className='w-5 h-5'>
+                        <img alt='' src={redux} />
+                    </div>
+                    <div class="min-w-0 flex-1 flex flex-col">
+                        <p class="truncatee">Redux</p>
                         {/* <small class="truncate">(Dart, Flutter, Firebase)</small> */}
                     </div>
 

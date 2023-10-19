de2034a4afe8ed83a3bd95c9f0208cf006f66888
alameen-alli <allialameen8739@gmail.com>
2023-10-19 15:53:56 +0100

    add resume icon and link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import myLogo from '../image/b541711059847543.62750747823ad.png';
-import linkedin from '../image/linkedin.png';
+import resume from '../image/resume.svg';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Transition } from 'react-transition-group';
 import { HashLink } from 'react-router-hash-link';
@@ -61,7 +61,7 @@ const Navbar = () => {
                                             <HashLink to="#about" smooth>About</HashLink>
                                             <HashLink to="#projects" smooth>Projects</HashLink>
                                             <HashLink to="#contact" smooth>Reach out</HashLink>
-                                            <a href='https://www.linkedin.com/in/allialameen/' to="#contact" smooth><img alt="" className='w-7 h-7' src={linkedin} /></a>
+                                            <a href='https://www.linkedin.com/in/allialameen/' to="#contact" smooth><img alt="" className='w-7 h-7' src={resume} /></a>
                                         </nav>
                                     </div>
                                 </div>
@@ -72,25 +72,11 @@ const Navbar = () => {
                 <nav className='nav hidden md:flex space-x-12'>
                     <HashLink to="#about" className='nav-gradient' smooth>About</HashLink>
                     <HashLink to="#projects" className='nav-gradient' smooth>Projects</HashLink>
-                    <HashLink to="#contact" className='nav-gradient' smooth>Reach out</HashLink>
-                    <a href="https://www.linkedin.com/in/allialameen/" className='nav-gradient'>
-                        {/* <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="#d1d5db">
-                            <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                        </svg> */}
-                        <img alt='' src={linkedin} className='w-7 h-7' />
+                    <HashLink to="#contact" className='nav-gradient' smooth>Contact</HashLink>
+                    <a href="https://drive.google.com/file/d/1SuTk1IQB1LrJL6Ds01meefTMn3prE4se/view?usp=share_link" className='nav-gradient'>
+                        <img alt='' src={resume} className='w-6 h-6' />
                     </a>
                 </nav>
-
-                {/* <div className='items-center justify-end hidden md:flex md:flex-1 lg:w-0'>
-                    <button>
-                        <div className='text-gray-300'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="#d1d5db">
-                                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                            </svg>
-                        </div>
-                    </button>
-                </div> */}
-
             </div>
         </div>
     )
